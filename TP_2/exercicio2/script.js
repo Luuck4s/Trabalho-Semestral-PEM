012852369d55d18fb695253cd3cdf5d6dd99451e
ALUNO ANDROID <aluno.android@ETECPG.local>
2019-11-08 21:18:07 -0300

    Tp2 [Exercicio 2,3]

--- TP_2/exercicio2/script.js
+++ TP_2/exercicio2/script.js
@@ -1,4 +1,18 @@
-function calcular(){
-    
+function calcular() {
+    var p = document.getElementById("resultado")
+    var count = 0
+    var numero = Number(window.prompt("Entre com um número"))
 
+
+    for (let i = 1; i <= numero; i++) {
+        if(numero%i == 0){
+            count++
+        }
+    }   
+
+    if(count == 2){
+        p.innerHTML = `${numero} é primo`
+    }else{
+        p.innerHTML = `${numero} não é primo`
+    }
 }
--- TP_2/exercicio3/script.js
+++ TP_2/exercicio3/script.js
@@ -1,43 +1,20 @@
 function calcular() {
-    var a = Number(document.getElementById("a").value)
-    var b = Number(document.getElementById("b").value)
-    var c = Number(document.getElementById("c").value)
     var p = document.getElementById("resultado")
-    
+    var aux, prox
+    var atual = 1
+    var ant = 0
+    var qtd = Number(window.prompt("Entre com a quantidade de termos"))
 
-    p.innerHTML = ""
-
-    if(a < b && a < c){
-        p.innerHTML += "<br>" + a 
-
-        if(b < c){
-            p.innerHTML += "<br>" + b 
-            p.innerHTML += "<br>" + c 
-        }else{
-            p.innerHTML += "<br>" + c
-            p.innerHTML += "<br>" + b 
-        }
-
-    }else if(b < c){
-        p.innerHTML += "<br>" + b
-
-        if(a < c){
-            p.innerHTML += "<br>" + a
-            p.innerHTML += "<br>" + c 
-        }else{
-            p.innerHTML += "<br>" + c
-            p.innerHTML += "<br>" + a 
-        }
-    }else{
-        p.innerHTML += "<br>" + c
-        if(a < b){
-            p.innerHTML += "<br>" + a
-            p.innerHTML += "<br>" + b
-        }else{
-            p.innerHTML += "<br>" + b
-            p.innerHTML += "<br>" + a
-        }
+    p.innerHTML += "1 "
+    for(let i = 0; i < qtd; i++){
+        prox = ant + atual
+        aux = ant
+        ant = atual 
+        atual = prox
+        
+        p.innerHTML += " " + prox 
     }
+
 }
 
 
